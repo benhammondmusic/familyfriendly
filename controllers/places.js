272184282e12023c66d5b92656a72961e46d6a69
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-01-31 22:01:17 -0700

    stores place author id in db

--- controllers/places.js
+++ controllers/places.js
@@ -12,7 +12,6 @@ const index = (req, res) => {
       if (err) return console.log(err);
 
       const context = { places, user: req.user };
-      console.log('context in ctrl.places.index before being sent to places/index view', context);
       // send to places/index view
       res.render('places/index', { context });
     });
@@ -27,71 +26,27 @@ const newPlaceForm = (req, res) => {
 // POST - create one new place
 const create = (req, res) => {
   console.log('CREATE PLACE');
-  const userId = req.session;
-
-  // get place name from submitted form on POST /places
-  const placeName = req.body.name;
-
-  //  var user = new User({
-  //      "loc": {
-  //          "type": "Point",
-  //          "coordinates": [-73.97, 40.77]
-  //      }
-  //  });
 
   // create a new place
   db.Place.create(req.body, (err, createdPlace) => {
     if (err) return console.log(err);
 
-    // find authoring user
-    db.User.findById(userId, (err, foundUser) => {
-      // save place with author ID
-      // createdPlace.authorUserId = foundUser._id;
-      createdPlace.name = placeName;
+    // get new place author ID from current logged in user
+    createdPlace.authorUserId = req.session.passport.user;
+    // get new place name from submitted form on POST /places
+    createdPlace.name = req.body.name;
 
-      // place geo location
-      createdPlace.loc.type = 'Point';
-      // ! need get user lat/long
-      createdPlace.loc.coordinates = [1000, 1000];
-      createdPlace.save();
+    // place geo location
+    createdPlace.loc.type = 'Point';
+    // ! need get user lat/long
+    createdPlace.loc.coordinates = [1000, 1000];
 
-      // update author places with new place ID
-      // ! foundUser.authoredPlaces.push(createdPlace._id);
-      // ! foundUser.save();
+    // save to DB
+    createdPlace.save();
 
-      // ! need send GET /places to display all places when done
-      res.redirect('/');
-    });
+    res.redirect('places');
   });
 };
-
-// const newPost = ( req, res ) => {
-//   const userId = req.session.currentUser.userId;
-
-//   db.Post.create( req.body, ( err, createdPost ) => {
-//     if ( err ) return console.log(err)
-
-//     db.User.findById( userId, ( err, foundUser ) => {
-
-//       createdPost.user = foundUser._id;
-//       createdPost.save();
-
-//       foundUser.posts.push(createdPost._id);
-//       foundUser.save();
-
-//       res.redirect('/');
-//     });
-//   });
-// }
-
-// const index = (req, res, next) => {
-//   Place.find({}, function (err, places) {
-//     res.render('places/index', {
-//       places,
-//       user: req.user,
-//     });
-//   });
-// };
 
 module.exports = {
   index,
