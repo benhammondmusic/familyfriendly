9bead8a8492de3639fbcbb79fdce7599545901f7
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-02-01 12:53:45 -0700

    creates reportcard in db with userID and placeID objs

--- controllers/reportCards.js
+++ controllers/reportCards.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 // database index which imports places, users, etc
 const db = require('../models');
 
@@ -27,8 +29,31 @@ const newReportCardForm = (req, res) => {
 };
 
 // CREATE REPORT CARD IN DB
+// POST - from views/reportcards/new
 const create = (req, res) => {
-  console.log('CREATE REPORT CARD IN DB');
+  console.log('CREATE REPORT CARD');
+  //   console.log(req.body);
+
+  // create a new report card with req.body containing user form input
+  db.ReportCard.create(req.body, (err, createdReportCard) => {
+    if (err) return console.log(err);
+
+    // set reportcard authorID to current logged in userID
+    createdReportCard.authorUserId = req.session.passport.user;
+
+    // get place ID OBJECT from request params ID STRING
+    createdReportCard.place = mongoose.Types.ObjectId(req.params.id);
+
+    // save to DB
+    createdReportCard.save();
+
+    // send user and all report cards along to view
+    const context = {
+      user: req.user,
+    };
+
+    res.render('reportcards/', context);
+  });
 };
 
 // const newPost = ( req, res ) => {
