f08680047a21395d2b6634fd23f11c499d14b874
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-02-03 19:42:41 -0700

    simplified models, updated ERG

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 
 // user authentication middleware
 app.use((req, res, next) => {
-  app.locals.currentUser = req.session.currentUser;
+  app.locals.user = req.session.user;
   next();
 });
 
